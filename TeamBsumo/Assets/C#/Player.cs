3065f93e0d95e52b8ba150c1669c675d9861035c
18b26 <1>
2020-09-04 14:54:01 +0900

    Playerに変数定義

--- TeamBsumo/Assets/C#/Player.cs
+++ TeamBsumo/Assets/C#/Player.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    struct PlayerInfo
+    {
+        public int HP;          //プレイヤーの体力
+        public int MP;          //プレイヤーの必殺技ゲージ
+        public int Stamina;     //プレイヤーのスタミナ
+    }
+
+    enum PlayerState
+    {
+        preparation,
+        startdash,
+        normal,
+        guard,
+        fling,
+        normalattack,
+        damage,
+        
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
