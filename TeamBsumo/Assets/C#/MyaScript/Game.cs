80db4724cb84dd17bb74242d50d6ba63341bdf0c
18b26 <1>
2020-09-09 14:59:00 +0900

    Plyaer class 変更

--- TeamBsumo/Assets/C#/MyaScript/Game.cs
+++ TeamBsumo/Assets/C#/MyaScript/Game.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public Player m_player;
+    public Player m_player_01;
+    public Player m_player_02;
+    public Player m_player_03;
+
+    public Player firstPlayer;
+    public Player secondPlayer;
+
+    public void SelectButton()
+    {
+        firstPlayer =Instantiate(m_player_01,new Vector3(-100.0f,10.0f,40.0f), Quaternion.identity);
+        secondPlayer =Instantiate(m_player_03, new Vector3(100.0f, 10.0f, 40.0f), Quaternion.identity);
+
+        firstPlayer.gameObject.GetComponent<Player>().SetEnemy(secondPlayer);
+        secondPlayer.gameObject.GetComponent<Player>().SetEnemy(firstPlayer);
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(m_player);
+       
+
        // m_player.gameObject.
     }
 
