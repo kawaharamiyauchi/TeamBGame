ddc55c271c6993a48557906e6435486fb9c02ed2
18b26 <1>
2020-09-09 11:18:30 +0900

    フォルダー追加 Player　class変更

---
+++ TeamBsumo/Assets/C#/MyaScript/Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public Player m_player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(m_player);
+       // m_player.gameObject.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TeamBsumo/Assets/C#/MyaScript/MoveControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl : MonoBehaviour
+{
+
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController charaCon =
+           gameObject.GetComponent<CharacterController>();
+
+        Vector3 move = new Vector3();
+        //move.x = 0.1f;
+
+        MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            move.x = -10.0f;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            move.x = 10.0f;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            move.y += 50.0f;
+        }
+        move.y -= 4.0f;
+        charaCon.Move(move);
+
+        //Star star = GameObject.FindObjectOfType<Star>();
+        //GameObject god = GameObject.Find("God");
+        //God godScript = god.GetComponent<God>();
+        //godScript.numGetStar++;
+    }
+}
