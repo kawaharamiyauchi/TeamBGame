466ddc27a20a91b89fc042c55788f8bb871e5fad
wada <1>
2020-09-10 14:11:39 +0900

    カメラを考慮してプレイヤーを移動させる カメラの移動処理

--- TeamBsumo/Assets/C#/Camera.cs
+++ TeamBsumo/Assets/C#/Camera.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    [SerializeField, HeaderAttribute("ゲームオブジェクト")]
     public GameObject stPlayer; //1P
     public GameObject ndPlayer; //2P
     private GameObject c_camera; //カメラ
@@ -15,6 +16,8 @@ public class Camera : MonoBehaviour
 
     private Vector3 cameraposition; //カメラの座標
     public float kyori; //近平面
+
+    public float DistanceToPlayerM = 2f;
 
     //プレイヤー同士の距離計算処理(distanceに値が代入される)
     void PlayerDistance(Vector3 stPl,Vector3 ndPl)
@@ -41,11 +44,76 @@ public class Camera : MonoBehaviour
         midpoint = m;
     }
 
+    //カメラ移動処理
+    void TpsCameraMove(Vector3 midpoint)
+    {
+        //ステップが追加されたら回転する処理をマウス移動から変更する
+        //マウスの移動を検知
+        //var rotX = Input.GetAxis("Mouse X") * Time.deltaTime * 100f;
+        //var rotY = Input.GetAxis("Mouse Y") * Time.deltaTime * 100f;
+
+        var lookAt = midpoint + Vector3.up * 1.2f;
+        //回す
+        //transform.RotateAround(lookAt, Vector3.up, rotX);
+
+        //カメラの場所をプレイヤー間の距離に応じて変える
+        if (distance > kyori)
+        {
+            DistanceToPlayerM = (float)distance;
+        }
+
+        // カメラとプレイヤーとの間の距離を調整
+        transform.position = lookAt - transform.forward * DistanceToPlayerM;
+
+        // カメラを横にずらして中央を開ける
+        transform.position = transform.position + transform.right * 0.0f;
+
+        //中点に視点を向ける
+        c_camera.transform.LookAt(lookAt);
+    }
+
+
+    //確認用
+    public GameObject MIDPOINT;
+    void CameraMove()
+    {
+        //MIDPOINTをプレイヤー間に置く
+        MIDPOINT.transform.position = midpoint;
+        if (Input.GetButton("Jump"))
+        {
+            MIDPOINT.transform.Rotate(new Vector3(0, 1, 0));
+        }
+        var lookAt = midpoint + Vector3.up;
+        // カメラとプレイヤーとの間の距離を調整
+        transform.position = lookAt - transform.forward * DistanceToPlayerM;
+        if (distance > kyori)
+        {
+            DistanceToPlayerM = (float)distance;
+        }
+        //中点に視点を向ける
+        c_camera.transform.LookAt(lookAt);
+
+    }
+    //確認オワリ
+
     // スタート関数（最初に一回だけ呼び出される）
-    void Start()
+    private void Start()
     {
         c_camera = this.gameObject;
+
+        //確認用
+        Vector3 cameraPos = MIDPOINT.transform.position;
+
+        //カメラを少し後ろに少し高く億
+        cameraPos.z -= 3.0f;
+        cameraPos.y += 1.0f;
+
+        c_camera.transform.position = cameraPos;
+
+        //親子関係を結ぶ
+        c_camera.transform.parent = MIDPOINT.transform;
     }
+
 
     // アップデート関数（1フレームに一回呼び出される）
     void Update()
@@ -54,28 +122,24 @@ public class Camera : MonoBehaviour
         stPlayerPosition = stPlayer.transform.position;
         ndPlayerPosition = ndPlayer.transform.position;
 
-        //カメラの座標を更新
-        cameraposition = c_camera.transform.position;
+        ////カメラの座標を更新
+        //cameraposition = c_camera.transform.position;
 
         //プレイヤー同士の距離を計算する
         PlayerDistance(stPlayerPosition, ndPlayerPosition);
+
         //プレイヤー間の中点を計算する
         PlayerMidPoint(stPlayerPosition, ndPlayerPosition);
-    
-        //カメラの場所をプレイヤー間の距離に応じて変える
-        if (distance > kyori)
-        {
-            cameraposition.z = (float)distance * -1;
-        }
-        Debug.Log(distance);
 
-        //プレイヤー間の中点に移動する
-        cameraposition.x = midpoint.x;
+        CameraMove();
 
 
-        //カメラを更新
-        c_camera.transform.position = cameraposition;
 
-        c_camera.transform.LookAt(midpoint);
+        //カメラの回転処理
+        //TpsCameraMove(midpoint);
+
+        //中点に視点を向ける
+        //c_camera.transform.LookAt(midpoint);
+
     }
 }
--- TeamBsumo/Assets/C#/PlayerMove.cs
+++ TeamBsumo/Assets/C#/PlayerMove.cs
@@ -13,6 +13,12 @@ public class PlayerMove : MonoBehaviour
     private Animator anim;
     private float x, y;
 
+
+
+    public GameObject c_camera;
+
+
+
     [SerializeField, HeaderAttribute("移動速度")]
     private float speed = 3.0f;
 
@@ -39,5 +45,21 @@ public class PlayerMove : MonoBehaviour
     void FixedUpdate()
     {
         rig.velocity = new Vector3(x, 0, y) * speed;
+
+        // カメラの方向から、X-Z平面の単位ベクトルを取得
+        Vector3 cameraForward = Vector3.Scale(c_camera.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        // 方向キーの入力値とカメラの向きから、移動方向を決定
+        Vector3 moveForward = cameraForward * y + c_camera.transform.right * x;
+
+        // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+        rig.velocity = moveForward * speed + new Vector3(0, rig.velocity.y, 0);
+
+        // キャラクターの向きを進行方向に
+        if (moveForward != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveForward);
+        }
+
     }
 }
